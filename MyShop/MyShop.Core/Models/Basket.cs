c913545eaee8538abbaf2ac36481fe8d7e782eb4
Bnguyen77 <bng7761@gmail.com>
2019-02-23 19:45:45 -0600

    Add BasketModel and BasketItems Model Add new DBSet

---
+++ MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Basket: BaseEntity
+    {
+        //Lazy Loading (virtual)
+        //whenever Basket is loaded, entity Framework will also load the Basket Item.
+
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();           
+        }
+    }
+}
